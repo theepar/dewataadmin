2bbd93f304f61d0361080f3f9e5acc46f0ea39b4
Deva <threpasty@gmail.com>
2025-08-14 20:00:22 +0800

    test cronjob

--- cronjob.php
+++ cronjob.php
@@ -8,6 +8,9 @@ exec('git pull origin main 2>&1', $output, $return_var);
 echo "Pulling latest code from origin/main...\n";
 echo implode("\n", $output);
 
+$logFile = __DIR__ . '/cronjob.log';
+file_put_contents($logFile, "[".date('Y-m-d H:i:s')."] Cronjob dijalankan\n", FILE_APPEND);
+
 if ($return_var === 0) {
     echo "\nGit pull sukses!\n";
 } else {
