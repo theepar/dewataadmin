12ed25e4bf473f3ca3773df3dd9754d25c445fef
Deva <threpasty@gmail.com>
2025-08-14 20:26:02 +0800

    push

--- cronjob.sh
+++ cronjob.sh
@@ -2,14 +2,3 @@
 
 LOGFILE="$(dirname "$0")/cronjob.log"
 php artisan schedule:work >> "$LOGFILE" 2>&1
-
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] Cronjob dijalankan" >> "$LOGFILE"
-
-echo "Pulling latest code from origin/main..."
-git pull origin main >> "$LOGFILE" 2>&1
-
-if [ $? -eq 0 ]; then
-    echo "Git pull sukses!" >> "$LOGFILE"
-else
-    echo "Git pull gagal!" >> "$LOGFILE"
-fi
