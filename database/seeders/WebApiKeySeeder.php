2d1ce2400078e6027789699f1427dfd87d8e24de
Deva <threpasty@gmail.com>
2025-08-16 14:46:40 +0800

    push seeder

--- database/seeders/WebApiKeySeeder.php
+++ database/seeders/WebApiKeySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use Illuminate\Support\Str;
 
 class WebApiKeySeeder extends Seeder
 {
@@ -13,11 +14,11 @@ class WebApiKeySeeder extends Seeder
         $admin = User::role('admin')->first();
         if ($admin) {
             DB::table('web_api_keys')->insert([
-                'user_id' => $admin->id,
+                'user_id'      => $admin->id,
                 'website_name' => 'nextjs',
-                'key' => '01d3a112b1d13607ef488f25e722e3f8879aaef9fbc0d3c4',
-                'created_at' => now(),
-                'updated_at' => now(),
+                'key'          => Str::uuid(), // atau Str::random(32)
+                'created_at'   => now(),
+                'updated_at'   => now(),
             ]);
         }
     }
